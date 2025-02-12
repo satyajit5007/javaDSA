1fb01dfba2a72c14d0a39a5355419a4263607026
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-02-13 00:01:21 +0530

    array

--- src/arrayPractice/arraytest1.java
+++ src/arrayPractice/arraytest1.java
@@ -72,7 +72,6 @@ public class arraytest1 {
         int [] arr1 = {1,2,3,4,5};
         for (int i = 0 ; i< arr1.length ; i++){
             System.out.println(arr1[i]);
-            System.out.println();
         }
 
 
