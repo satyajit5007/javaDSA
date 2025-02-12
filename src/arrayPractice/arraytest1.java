789f0d288254b6848277e4d70372466313b61ed9
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-02-12 23:53:49 +0530

    xml

--- src/arrayPractice/arraytest1.java
+++ src/arrayPractice/arraytest1.java
@@ -68,6 +68,12 @@ public class arraytest1 {
             System.out.println();
         }
 
+        System.out.println("---------------------------------------------");
+        int [] arr1 = {1,2,3,4,5};
+        for (int i = 0 ; i< arr1.length ; i++){
+            System.out.print(arr1[i]);
+        }
+
 
     }
 }
