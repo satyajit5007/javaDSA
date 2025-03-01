5c565e277087c559c976c8ae64d8ef4f29244ced
satyajit5007 <bhosalesatyajit5007@gmail.com>
2025-03-01 11:02:16 +0530

    array test

---
+++ src/arrayPractice/array2.java
@@ -0,0 +1,26 @@
+package arrayPractice;
+
+public class array2 {
+    public static void main(String[] args) {
+
+
+
+//        int [] arr1 = new int[5];
+//        arr1[0]=1;
+//        arr1[1]=2;
+//        arr1[2]=3;
+//        arr1[3]=4;
+//        arr1[4]=5;
+//
+//        for (int index =  0 ; index< arr1.length; index++){
+//            System.out.print(" "+arr1[index]);
+//        }
+
+        int [] arr2 = {1,2,3,4,5};
+        for (int index = 0 ;  index< arr2.length; index++){
+            System.out.print(arr2[index]+" ");
+        }
+
+    }
+
+}
